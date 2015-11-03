872ebee2461c0beafca294edb3ea3d335dbef054
erueger1 <erueger1@smail.uni-koeln.de>
2015-11-03 11:31:36 +0100

    helloworld nachricht

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		String s = "Hello World";
+		
+		System.out.println(s);
+	System.out.println("Hello World");
+				
+				
+
+	}
+}
+    
